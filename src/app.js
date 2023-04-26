a5e219c9266f658999c432ea7199920e87fc6927
lucasbicudo <bicudo.aguiar@gmail.com>
2023-04-26 20:32:48 -0300

    crud GET, POST, PUT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 
 const books = [
   { id: 1, title: "Em busca de Sentido" },
@@ -7,9 +8,31 @@ const books = [
   { id: 3, title: "Começe pelo porque" },
 ];
 
+// CRUD Routes
 app.get("/", (req, res) => res.send("I'm Learning Node.js"));
 app.get("/books", (req, res) => {
   res.status(200).send(books);
 });
+
+app.put("/books/:id", (req, res) => {
+  let find = findBook(req.params.id);
+  res.status(200).json(books[find]);
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.status(200).send("Book created with sucessful!");
+});
+
+app.put("/books/:id", (req, res) => {
+  let find = findBook(req.params.id);
+  books[find] = req.body;
+  res.status(200).send("Updated was sucessful");
+});
+
+function findBook(id) {
+  return books.findIndex((book) => book.id == id);
+}
+//cre
 
 export default app;
