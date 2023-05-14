459cad846083f91d007311edfefe5f84179bd4d3
lucasbicudo <bicudo.aguiar@gmail.com>
2023-05-13 22:29:08 -0300

    ajustes na classe de books

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -16,7 +16,8 @@ class BookController {
       const id = req.params.id;
       const book = await books.findById(id);
       if (!book) {
-        next(new ErrorNotFound('ID não encontrado'));
+        console.log(book);
+        next(new ErrorNotFound('ID do livro não encontrado'));
       } else {
         return res.status(200).send(book);
       }
@@ -29,7 +30,6 @@ class BookController {
     try {
       const book = new books(req.body);
       await book.save();
-
       return res.status(201).send(book.toJSON());
     } catch (err) {
       next(err);
@@ -40,54 +40,67 @@ class BookController {
     try {
       const id = req.params.id;
       const dataBook = req.body;
-      await books
-        //.findByIdAndUpdate(id, { $set: req.body }) You can use this way
-        .findByIdAndUpdate(id, dataBook);
-      return res.status(200).send(`Livro ${id} atualizado com sucesso!`);
+      //.findByIdAndUpdate(id, { $set: req.body }) You can use this way
+      const returnUpdate = await books.findByIdAndUpdate(id, dataBook);
+      if (returnUpdate !== null) {
+        return res.status(200).send(`Livro ${id} atualizado com sucesso!`);
+      } else {
+        next(
+          new ErrorNotFound(
+            'ID do livro não encontrado - Verifique o ID, da URL'
+          )
+        );
+      }
     } catch (err) {
       next(err);
     }
   };
 
-  static deleteBook = async (req, res) => {
-    const id = req.params.id;
-    await books
-      .findByIdAndDelete(id)
-      .then(() => {
+  static deleteBook = async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const returnDelete = await books.findByIdAndDelete(id);
+      if (returnDelete !== null) {
         return res.status(200).send(`Livro ${id} deletado com sucesso`);
-      })
-      .catch((err) => {
-        return res.status(500).send({ message: `${err.message} => Deu erro` });
-      });
+      } else {
+        next(
+          new ErrorNotFound(
+            'ID do livro não encontrado - Verifique o ID, da URL'
+          )
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
   };
 
   static listBookByPublisher = async (req, res) => {
-    const publisher = req.query.publisher;
-    await books
-      .find({ editor: publisher })
-      .populate('author')
-      .then((returna) => {
-        res.status(200).send(returna);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
+    try {
+      const publisher = req.query.publisher;
+      const returnList = await books
+        .find({ editor: publisher })
+        .populate('author');
+      res.status(200).send(returnList);
+    } catch (err) {
+      res.send(err);
+    }
   };
 
-  static listBookByPublisherId = async (req, res) => {
-    const editor = req.params.id;
-    await books
-      .find({ editor: { _id: editor } })
-      .populate('author')
-      .populate('editor')
-      .then((returna) => {
-        res.status(200).send(returna);
-      })
-      .catch(() => {
-        res.status(404).send({
-          messageErr: `id => ${editor} não foi encontrado, Digite um id correto`,
-        });
-      });
+  static listBookByPublisherId = async (req, res, next) => {
+    try {
+      const editor = req.params.id;
+      const returnList = await books
+        .find({ editor: { _id: editor } })
+        .populate('author')
+        .populate('editor');
+      if (Object.keys(returnList).length !== 0) {
+        res.status(200).send(returnList);
+      } else {
+        next(new ErrorNotFound('ID do Editor não encontrado'));
+      }
+    } catch (err) {
+      next(err);
+    }
   };
 }
 
